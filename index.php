261d9eadada3fe255eb66a89ce40f96137d64aa8
Riski <118254486+Riski2208@users.noreply.github.com>
2022-12-16 11:54:03 +0700

    c

--- index.php
+++ index.php
@@ -6,10 +6,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/bootstrap.min.css">
-    <title>Document</title>
+    <link rel="stylesheet" href="css/css_custom.css">
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css'
+        integrity='sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=='
+        crossorigin='anonymous' />
+    <title>Catering </title>
 </head>
 
 <body>
+
 
 
     <script src="js/bootstrap.bundle.min.js"></script>
